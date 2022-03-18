c88cb7996a14fbd1069ea151e1b58ada4b6930f8
hungle546 <56492292+hungle546@users.noreply.github.com>
2022-03-18 14:14:02 -0700

    UI changes
    feat: Raycasting for buttons. Button script ready to use. Settings, start, and Algorithm button in main menu

---
+++ VR/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public void StartClicked()
+    {
+        Debug.Log("start button clicked");
+    }
+
+    public void AlgorClicked()
+    {
+        Debug.Log("algorithm button clicked");
+        
+    }
+
+    public void SettingClicked()
+    {
+        Debug.Log("settings Clicked");
+    }
+}
