e7ce36ed557df22ca10d2f168c63c5fafb964376
hungle546 <56492292+hungle546@users.noreply.github.com>
2022-03-18 21:42:57 -0700

    rename files and created new scene
    feat: Made algorithm scene, with astar and dijkstra algo

--- VR/Assets/Scripts/MainMenuButtons.cs
+++ VR/Assets/Scripts/MainMenuButtons.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MainMenuButtons : MonoBehaviour
 {
     public void StartClicked()
@@ -12,7 +12,8 @@ public class MainMenuButtons : MonoBehaviour
     public void AlgorClicked()
     {
         Debug.Log("algorithm button clicked");
-        
+        SceneManager.LoadScene(1);
+
     }
 
     public void SettingClicked()
@@ -20,3 +21,5 @@ public class MainMenuButtons : MonoBehaviour
         Debug.Log("settings Clicked");
     }
 }
+
+
